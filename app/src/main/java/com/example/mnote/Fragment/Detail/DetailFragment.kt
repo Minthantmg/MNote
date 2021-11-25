1d78ee648b6f9db993f89206e090b2784f99d373
lenovo <minthant180@gmail.com>
2021-11-25 21:37:45 +0630

    added data to room Database

--- app/src/main/java/com/example/mnote/Fragment/Detail/DetailFragment.kt
+++ app/src/main/java/com/example/mnote/Fragment/Detail/DetailFragment.kt
@@ -1,12 +1,23 @@
 package com.example.mnote.Fragment.Detail
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextUtils
 import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.example.mnote.R
+import com.example.mnote.model.User
+import com.example.mnote.viewModel.TextViewModel
+import kotlinx.android.synthetic.main.fragment_detail.*
+import kotlinx.android.synthetic.main.fragment_detail.view.*
+import kotlinx.android.synthetic.main.fragment_list.*
 
 class DetailFragment : Fragment() {
+
+    private lateinit var mViewModel : TextViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -14,9 +25,30 @@ class DetailFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_detail, container, false)
+        mViewModel = ViewModelProvider(this).get(TextViewModel::class.java)
+        view.add_btn.setOnClickListener {
+            insertDataToDatabase()
+        }
+
 
         setHasOptionsMenu(true)
         return view
+    }
+
+    private fun insertDataToDatabase() {
+        val title = title_et.text.toString()
+        val label = label_et.text.toString()
+        val text = text_et.text
+
+        if(inputCheck(title,label,text)){
+            val texts = User(0,title,label,text.toString())
+            mViewModel.addTitle(texts)
+            Toast.makeText(requireContext(),"Successfully added!",Toast.LENGTH_LONG).show()
+            findNavController().navigate(R.id.action_detailFragment_to_listFragment)
+        }else{
+            Toast.makeText(requireContext(),"Please fill all Fields!!",Toast.LENGTH_LONG).show()
+        }
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
@@ -32,6 +64,10 @@ class DetailFragment : Fragment() {
 
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    private fun inputCheck(title : String,label : String,text : Editable):Boolean{
+        return !(TextUtils.isEmpty(title) && TextUtils.isEmpty(label) && text.isEmpty())
     }
 
 
